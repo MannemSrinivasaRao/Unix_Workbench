ae26e3a86be81d1f92f8f0d7e6037c0d6a4b9800
MannemSrinivasaRao <36668667+MannemSrinivasaRao@users.noreply.github.com>
2020-09-09 16:42:19 +0530

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Reading number of files in directory
+
+Number_of_files=`ls | wc -l`
+function files_count()
+{
+  echo "How many files do you guess are there in the current directory:"
+  read response
+  
+  #checking whether guessed number matches with existing files
+  
+  while [ $response -ne $Number_of_files ]
+  do
+     if [ $response -gt $Number_of_files ]
+     then
+      echo "Guessed value is higher than the existing files, guess again:"
+     else
+      echo "Guessed value is lower than the existing files, guess again:"
+     fi
+     read response
+  done
+  echo "Congratulations! You have guessed the exact number of files"
+}
+files_count()
